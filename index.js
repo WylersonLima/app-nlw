cff0931263a499eb780ed3e1a679a01a364519e9
Wylerson Lima <wylersonlima@icloud.com>
2024-09-13 11:55:09 -0400

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = {
     value: 'Tomar 4L de água por dia',
@@ -17,6 +17,34 @@ const cadastrarMeta = async () => {
 
     metas.push({ value: meta, checked: false })
 
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox ({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    if(respostas.length == 0){
+        console.log("nenhuma meta selecionada!")
+        return
+    }
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log("Meta(s) concluída(s)")
+    
 }
 
 async function start() {
@@ -48,7 +76,7 @@ async function start() {
                 console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")
+                await listarMetas()
                 break
             case "sair":
                 console.log("Até a próxima!")
